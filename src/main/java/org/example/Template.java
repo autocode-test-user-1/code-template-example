68071c4d78989ebc292feacc45dabe9a586e902d
Mykhailo_Beheka <Mykhailo_Beheka@epam.com>
2023-07-26 12:15:07 +0300

    Test commit release

--- src/main/java/org/example/Template.java
+++ src/main/java/org/example/Template.java
@@ -1,7 +1,6 @@
 package org.example;
 
 public class Template {
-
     public String reverseString(String str) {
         StringBuilder builder = new StringBuilder(str);
         return builder.reverse().toString();
--- src/test/java/org/example/TemplateTest.java
+++ src/test/java/org/example/TemplateTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TemplateTest {
-
+//release
     @ParameterizedTest
     @ValueSource(strings = {"abc", "123", "Hello world!"})
     @DisplayName("Reverse test")
