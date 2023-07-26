3ee776db2d3b6b64193473bdb720453ab3f96d35
Mykhailo_Beheka <Mykhailo_Beheka@epam.com>
2023-07-26 12:10:01 +0300

    Test commit

--- src/main/java/org/example/Template.java
+++ src/main/java/org/example/Template.java
@@ -3,7 +3,17 @@ package org.example;
 public class Template {
 
     public String reverseString(String str) {
+        StringBuilder builder = new StringBuilder(str);
+        return builder.reverse().toString();
+    }
 
-        return str;
+    public static boolean isPalindrome(int x) {
+        var intToString = String.valueOf(x);
+        for (int i = 0; i < intToString.length() - 1; i++) {
+            if (intToString.charAt(i) != intToString.charAt(intToString.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
