83e1a43110413fc1a7a80549d189e746895e995f
autocode-test-user-1 <70530690+autocode-test-user-1@users.noreply.github.com>
2023-08-01 14:30:04 +0000

    student-solution-comit

--- src/main/java/org/example/Template.java
+++ src/main/java/org/example/Template.java
@@ -5,16 +5,4 @@ public class Template {
         StringBuilder builder = new StringBuilder(str);
         return builder.reverse().toString();
     }
-
-    public static boolean isPalindrome(int x) {
-        var intToString = String.valueOf(x);
-        for (int i = 0; i < intToString.length() - 1; i++) {
-            if (intToString.charAt(i) != intToString.charAt(intToString.length() - 1 - i)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //feature
 }
